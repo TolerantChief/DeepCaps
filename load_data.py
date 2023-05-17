b9ae91653d1f381a2a506ce97cae014898018958
TolerantChief <juanjoselp1@gmail.com>
2023-05-17 17:21:04 +0200

    Added custom dataset.

--- load_data.py
+++ load_data.py
@@ -2,11 +2,56 @@
 Dataset loading and data transformation classes.
 '''
 
+import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import torchvision
+import os
 
 
+class Jamones:
+    def __init__(self, data_path, batch_size, shuffle, num_workers=4, rotation_degrees=30, translate=(0,0.2), scale=(0.95, 1.2)):
 
+        self.data_path = data_path
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.num_workers = num_workers
+        self.rotation = rotation_degrees
+        self.translate = translate
+        self.scale = scale
+        self.img_size = 28
+        self.num_class = 26
+
+    def __call__(self):
+
+        transform = transforms.Compose([
+        # shift by 2 pixels in either direction with zero padding.
+        transforms.Grayscale(),
+        transforms.Resize((self.img_size,self.img_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        transforms.RandomGrayscale(p=0.1),
+        transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.8, 1.2), shear=10),
+        transforms.RandomPerspective(distortion_scale=0.1, p=0.5),
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0)),
+        transforms.RandomRotation(10),
+        transforms.RandomCrop(self.img_size, padding=2),
+        transforms.ToTensor(),
+        #transforms.Normalize(mean, std)
+    ])
+
+        test_size = 0.2
+        dataset = torchvision.datasets.ImageFolder(root=os.path.join(self.data_path, 'JAMONES'), transform=transform)
+        num_data = len(dataset)
+        num_test = int(test_size * num_data)
+        num_train = num_data - num_test
+        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [num_train, num_test])
+        
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+
+
+        return train_loader, test_loader, self.img_size, self.num_class
 
 class FashionMNIST:
 
