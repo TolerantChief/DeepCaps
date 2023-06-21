da1847b837a32d2d6ac8e17c1735cfd9cfb5977a
TolerantChief <juanjoselp1@gmail.com>
2023-06-21 18:03:06 +0200

    Display number of parameters

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ import cfg
 from torchbearer.callbacks import EarlyStopping
 from mem_profile import get_gpu_memory_map
 import matplotlib.pyplot as plt
+from numpy import prod
 
 
 
@@ -32,6 +33,9 @@ def train(img_size, device=torch.device('cpu'), learning_rate=1e-3, num_epochs=5
     checkpoint_path = checkpoint_folder + checkpoint_name
 
     deepcaps = DeepCapsModel(num_class=num_classes, img_height=img_size, img_width=img_size, device=device).to(device) #initialize model
+
+    print('Num params:', sum([prod(p.size())
+              for p in deepcaps.parameters()]))
 
     #load the current checkpoint
     if load_checkpoint and not checkpoint_name is None and os.path.exists(checkpoint_path):
